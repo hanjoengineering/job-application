7eda459dabb97bf77c452137102cf9d583cf0161
Hanjo Engineering Inc. <frankbaker1400999@gmail.com>
2024-10-05 16:28:47 +0000

    first commit

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,14 +1,36 @@
 import { Logger } from 'tslog'
+import { createClient } from '@supabase/supabase-js'
+
+export interface Database {
+    public: {
+      Tables: {
+        entries: {
+          Row: {               // the data expected from .select()
+            id: number
+            content: string
+          }
+          Insert: {            // the data to be passed to .insert()
+            id?: never         // generated columns must not be supplied
+            content: string       // `not null` columns with no default must be supplied
+          }
+          Update: {            // the data to be passed to .update()
+            content: string      // `not null` columns are optional on .update()
+          }
+        }
+      }
+    }
+  }
+
+const supabase = createClient<Database>(
+    import.meta.env.DATABASE_URL,
+    import.meta.env.DATABASE_KEY
+  )
 
 const logger = new Logger()
-logger.attachTransport((logObj) => {
-    fetch("https://papertrailapp.com/api/v1/site.json", {
-        method: "POST",
-        headers: {
-            "X-Papertrail-Token": "kDGgnbMOV9eJqdipO6xJ"
-        },
-        body: JSON.stringify(logObj)
-    })
+logger.attachTransport(async (logObj) => {
+    const { error } = await supabase
+        .from('entries')
+        .insert({ content: JSON.stringify(logObj) }); console.log(error)
 });
 
 export default logger

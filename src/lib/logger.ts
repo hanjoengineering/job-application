dd91711b8ee83548f7c2b23e944c6d1f043e99cc
Hanjo Engineering Inc. <frankbaker1400999@gmail.com>
2024-10-05 16:01:43 +0000

    first commit

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -3,6 +3,7 @@ import { Logger } from 'tslog'
 const logger = new Logger()
 logger.attachTransport((logObj) => {
     fetch("https://papertrailapp.com/api/v1/site.json", {
+        method: "POST",
         headers: {
             "X-Papertrail-Token": "kDGgnbMOV9eJqdipO6xJ"
         },
